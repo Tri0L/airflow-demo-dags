a89b4a3ee5cfa14f8d896bf06eee74052420cb9d
Andrey Dudin <dudin.andrey@gmail.com>
2022-10-28 18:18:08 +0300

    new dags

---
+++ dags/dag4.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+import pendulum
+import time
+from airflow.decorators import task
+
+with DAG(
+    dag_id='dag4',
+    description='test dag',
+    schedule='@daily',
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    default_args={"owner": "@p.kozakov"},
+) as dummy_dag:
+
+    @task
+    def sleeper():
+        time.sleep(600) 
+    sleeper()
